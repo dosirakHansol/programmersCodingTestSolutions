a43d355b8603e9a18449632aa1eb4f55b3acf19d
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2022-09-09 21:39:57 +0900

    프로그래머스 코딩테스트 연습 2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천 정규식을 활용하여 문제 해결

---
+++ test01/src/recommendId/main.java
@@ -0,0 +1,15 @@
+package recommendId;
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		String new_id = "abcdefghijklmn.p";
+		
+		String re_id = new Solution().solution(new_id);
+		
+		System.out.println(re_id);
+		
+	}
+
+}
