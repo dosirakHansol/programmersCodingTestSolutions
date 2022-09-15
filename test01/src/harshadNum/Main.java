6991852d637534c7a281d4d8164557e24a5e672e
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2022-09-15 22:23:47 +0900

    하샤드 수 int -> String -> int 로 변환하는 과정

---
+++ test01/src/harshadNum/Main.java
@@ -0,0 +1,27 @@
+package harshadNum;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(solution(11));
+		
+	}
+	
+	public static boolean solution(int x) {
+        boolean answer = true;
+        
+        String num = Integer.toString(x);
+        int pNum = 0;
+        for(int i = 0; i < num.length(); ++i) {
+        	pNum += (num.charAt(i)-'0');
+        }
+        
+        if(x%pNum!=0) {
+        	answer = false;
+        }
+        
+        return answer;
+    }
+
+}
