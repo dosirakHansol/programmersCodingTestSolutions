37b4a693023be92bc648f5c213cbb2faf82daf5a
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2022-09-26 23:59:03 +0900

    없는 숫자 더하기 >> 아직 해결 X 코드 어느부분이 이상한지 체크중

--- test01/src/sumNotExistNum/Main.java
+++ test01/src/sumNotExistNum/Main.java
@@ -16,8 +16,11 @@ public class Main {
 	int i = 0;
 	
 	public static int solution(int[] numbers) {
+		
         int answer = 0;
+        
         Main a = new Main();
+        
         for(int i = 1; i < 11; ++i) {
         	answer += Arrays.stream(numbers).anyMatch(x -> x == a.i) ? 0 : i; 
         }
