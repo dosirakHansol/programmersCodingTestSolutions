7392b8da58d1675676828cb4a865749985b1ffc3
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-02-04 09:48:26 +0900

    짝지어 제거하기 - stack class 사용 - 반복 횟수가 훨씬 줄어듦

--- test02/src/removeTogether/Main.java
+++ test02/src/removeTogether/Main.java
@@ -3,11 +3,31 @@ package removeTogether;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(solution("baabaa"));
+		System.out.println(solutionStack("baabaa"));
+	}
+
+	static int solutionStack(String s) {
+	
+		int answer = -1;
+		
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+        }
+        answer = (stack.size() == 0) ? 1 : 0;
+        return answer;
 	}
 	
 	public static int solution(String s) {
