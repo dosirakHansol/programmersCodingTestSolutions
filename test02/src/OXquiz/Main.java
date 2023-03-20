d0962dc6fc7ecf84ba9494018f3bd6ad84b634f9
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-03-20 23:06:40 +0900

    OX 퀴즈

---
+++ test02/src/OXquiz/Main.java
@@ -0,0 +1,40 @@
+package OXquiz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] arr = {"3 - 4 = -3", "5 + 6 = 11"};
+		System.out.println(solution(arr));
+	}
+	
+	public static String[] solution(String[] arr) {
+		String[] answer = new String[arr.length];
+		
+		for(int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			int _sum = 0;
+			
+			String[] _arr = (arr[i].replaceAll(" ", "")).split("=");
+			
+			_sum = Integer.parseInt(_arr[1]);
+			
+			if(_arr[0].contains("+")) {
+				int x = Integer.parseInt(_arr[0].split("\\+")[0]);
+				int y = Integer.parseInt(_arr[0].split("\\+")[1]);
+				sum = x + y;
+			} else {
+				int x = Integer.parseInt(_arr[0].split("-")[0]);
+				int y = Integer.parseInt(_arr[0].split("-")[1]);
+				sum = x - y;
+			}
+			
+			if(sum == _sum) answer[i] = "O"; 
+			else answer[i] = "X";
+			System.out.println(answer[i]);
+		}
+		
+		return answer;
+	}
+
+}
