907b7a3330208d87a7329242ff05036482fd077d
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-03-22 23:22:29 +0900

    넓이가  M * N 인 종이 1*1 비율로 자를때 가위질 횟수 - 회식해서 쉬운문제로...

---
+++ test02/src/CutPapet/Main.java
@@ -0,0 +1,14 @@
+package CutPapet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int solution(int M, int N) {
+		return M * N - 1 ;
+	}
+
+}
