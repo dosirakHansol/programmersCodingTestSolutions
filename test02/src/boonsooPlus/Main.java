b85c646e6d49d5c6f4228a76c066a5ac3ca70dc2
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-04-14 23:56:10 +0900

    분수의 덧셈

---
+++ test02/src/boonsooPlus/Main.java
@@ -0,0 +1,26 @@
+package boonsooPlus;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numerator = (numer1 * denom2) + (numer2 * denom1); 
+        int denominator = denom1 * denom2;
+        
+        for(int i = numerator-1; i > 1; i--) {
+            // 기약분수로 만들기
+            if(numerator % i == 0 && denominator % i == 0) { 
+                numerator /= i;
+                denominator /= i;
+            }
+        }
+        
+        int[] answer = { numerator, denominator };
+        return answer;
+    }
+
+}
