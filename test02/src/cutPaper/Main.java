1050a792b21d649f41f9e14ec6021c3559ed95f6
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-06-06 23:09:40 +0900

    종이자르기!

---
+++ test02/src/cutPaper/Main.java
@@ -0,0 +1,16 @@
+package cutPaper;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int solution(int M, int N) {
+        int answer = 0;
+        answer = (M*N)-1;
+        return answer;
+    }
+
+}
